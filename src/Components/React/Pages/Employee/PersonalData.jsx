0ad599160aed0b299b7eaf8c05227ededac1dd31
Chukwunazaekpere <emmannuelchinaza5777@gmail.com>
2021-01-18 15:44:07 +0100

    first commit

---
+++ src/Components/React/Pages/Employee/PersonalData.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+
+
+const PersonalData = () => {
+    const fields = ['Last Name']
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+
+export default PersonalData;
